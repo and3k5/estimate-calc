232fddd80d239422eb11981a67a4ef21fb6bd8bf
Anders K <and3k5@users.noreply.github.com>
2021-01-13 14:36:59 +0100

    Make simple electron app work

--- electron/index.js
+++ electron/index.js
@@ -1,27 +1,3 @@
-const { app, BrowserWindow } = require('electron')
+const timeCalc = require("../lib/time-calc");
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    win.loadFile('index.html')
-}
-
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
+window.timeCalc = timeCalc;
