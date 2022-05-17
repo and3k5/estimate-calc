9ae47dc223c9e7f947733827d09b16b11aec9df2
Anders <and3k5@users.noreply.github.com>
2022-05-17 09:48:49 +0200

    Make notation a class

--- src/time/notations/index.ts
+++ src/time/notations/index.ts
@@ -1,4 +1,4 @@
-export interface Notation {
+export class Notation {
     notation: string;
     singleName: string;
     multiName: string;
@@ -6,72 +6,39 @@ export interface Notation {
     order: number;
     ms: number;
     relativeAmount: number;
-    prev: Notation;
-    next: Notation;
+    prev: Notation | undefined;
+    next: Notation | undefined;
     visibleByDefault: boolean;
-}
-const defNotations = [
-    // {
-    //     notation: "ms",
-    //     singleName: "millisecond",
-    //     multiName: "milliseconds",
-    //     propertyName: "milliseconds",
-    //     order: 4,
-    //     ms: 1,
-    //     relativeAmount: 1,
-    // },
-    {
-        notation: "s",
-        singleName: "second",
-        multiName: "seconds",
-        propertyName: "seconds",
-        order: 3,
-        ms: 1000,
-        relativeAmount: 1000,
-        visibleByDefault: false,
-    },
-    {
-        notation: "m",
-        singleName: "minute",
-        multiName: "minutes",
-        propertyName: "minutes",
-        order: 2,
-        ms: 1000 * 60,
-        relativeAmount: 60,
-        visibleByDefault: true,
-    },
-    {
-        notation: "h",
-        singleName: "hour",
-        multiName: "hours",
-        propertyName: "hours",
-        order: 1,
-        ms: 1000 * 60 * 60,
-        relativeAmount: 60,
-        visibleByDefault: false,
-    },
-    {
-        notation: "d",
-        singleName: "day",
-        multiName: "days",
-        propertyName: "days",
-        order: 0,
-        ms: 1000 * 60 * 60 * 24,
-        relativeAmount: 24,
-        visibleByDefault: false,
+    constructor(notation: string, singleName: string, multiName: string, propertyName: string, order: number, ms: number, relativeAmount: number, visibleByDefault: boolean) {
+        this.notation = notation;
+        this.singleName = singleName;
+        this.multiName = multiName;
+        this.propertyName = propertyName;
+        this.order = order;
+        this.ms = ms;
+        this.relativeAmount = relativeAmount;
+        this.visibleByDefault = visibleByDefault;
     }
-] as Notation[];
+}
+
+const defNotations: Notation[] = [
+    // new Notation("ms","millisecond","milliseconds","milliseconds",4,1,1,false),
+    new Notation("s","second","seconds","seconds",3,1000,1000,false),
+    new Notation("m","minute","minutes","minutes",2,1000 * 60,60,true),
+    new Notation("h","hour","hours","hours",1,1000 * 60 * 60,60,false),
+    new Notation("d","day","days","days",0,1000 * 60 * 60 * 24,24,false)
+];
 
 defNotations.forEach((val, i, arr) => val.prev = arr[i - 1]);
 defNotations.forEach((val, i, arr) => val.next = arr[i + 1]);
 
 export const defaultNotations = defNotations;
 
-export function getDefaultNotationByName(notationName : string) {
+export function getDefaultNotationByName(notationName: string) {
     return getNotationByName(defNotations, notationName);
 }
 
-export function getNotationByName(notations : Notation[], notationName : string) {
+export function getNotationByName(notations: Notation[], notationName: string) {
     if (!Array.isArray(notations))
         throw new Error("First argument must be an array");
     const notation = notations.filter(n => n.notation == notationName)[0];
